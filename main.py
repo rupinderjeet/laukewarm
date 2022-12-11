5998fc35ac481c65ab721a162166e268c9ede9e5
Rupinderjeet Singh Hans <rupinderjeet47@gmail.com>
2022-12-11 16:48:58 +0530

    restructure book_detail to receive html-file input and retrieve book-detail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
 
-from bs4 import BeautifulSoup
-
-from book_detail import read_book
+from reader.book_detail_html_reader import BookDetailHtmlReader
 from reader.book_list_html_reader import BookListHtmlReader
 
 
@@ -12,17 +10,13 @@ def load_book_list():
 
 
 def load_book_detail():
-    with open('sample/bookdetail.html') as file:
-        content = file.read()
-
-    document = BeautifulSoup(content, "html.parser")
-    book = read_book(document)
-    print(json.dumps(book, indent=2))
+    reader = BookDetailHtmlReader('sample/bookdetail.html')
+    print(json.dumps(reader.get_book_detail(), indent=2))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    load_book_list()
-    # load_book_detail()
+    # load_book_list()
+    load_book_detail()
     # download_book_list_pages()
     print('__done__')
